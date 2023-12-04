3eebb78993fe7b9db75228d3951b7eaade79ebc2
ChyoRi <chyori_@naver.com>
2023-12-04 17:54:45 +0900

    UpDate

--- tab/tab.js
+++ tab/tab.js
@@ -4,7 +4,6 @@ const tabContList = document.querySelectorAll('.tab-content');
 const btnClick = (e) => {
   let target = e.target;
   let idx = [...tabBtnList].indexOf(target);
-  console.log(idx)
 
   tabBtnList.forEach(item => {
     item.classList.remove('orange');
@@ -19,3 +18,11 @@ const btnClick = (e) => {
 }
 
 tabBtnList.forEach(item => item.addEventListener('click', btnClick));
+
+const itemNameTxt = document.querySelector('.item_name');
+const itemPriceTxt = document.querySelector('.item_price');
+
+const car2 = { name : '소나타', price : [50000, 3000, 4000]};
+
+itemNameTxt.innerHTML = car2.name;
+itemPriceTxt.innerHTML = car2.price[0];
