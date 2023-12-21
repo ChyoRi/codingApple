3e06191841d437284e5b25c152af3ed5582b1393
ChyoRi <chyori_@naver.com>
2023-12-21 17:59:50 +0900

    UpDate

--- tab/tab.js
+++ tab/tab.js
@@ -26,3 +26,38 @@ const car2 = { name : '소나타', price : [50000, 3000, 4000]};
 
 itemNameTxt.innerHTML = car2.name;
 itemPriceTxt.innerHTML = car2.price[0];
+
+const shirtForm = document.querySelector('.mt-4');
+const shirtSizeForm = document.querySelector('.shirt-size');
+const pantsSizeForm = document.querySelector('.pants-size');
+
+const pantsSize = ['28', '30'];
+
+const pantsSizeTag = `
+  <option>${pantsSize[0]}</option>
+  <option>${pantsSize[1]}</option>
+`;
+
+pantsSizeForm.insertAdjacentHTML('beforeend',pantsSizeTag);
+
+const formValue = (e) => {
+  let value = e.target.value;
+  
+  if(value === "shirt") {
+    shirtSizeForm.classList.add('show');
+  } else {
+    shirtSizeForm.classList.remove('show');
+  }
+  if (value === 'pants') {
+    pantsSizeForm.classList.add('show');
+  } else {
+    pantsSizeForm.classList.remove('show');
+  }
+}
+
+shirtForm.addEventListener('change', formValue);
+
+var a = document.createElement('p');
+a.innerHTML = '안녕';
+
+document.querySelector('#test').appendChild(a);
